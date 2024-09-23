87a188c1e6f8190741cc3696a71d9c363083dbca
shahalimshompod <shahalimsompod@gmail.com>
2024-09-23 18:12:36 +0600

    fixing some problems

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,3 @@
 document.getElementById('home-btn').addEventListener('click', function(){
-    window.location.href = 'https://shahalimshompod.github.io/b-10-assignment-5/'
+    window.location.href = 'https://shahalimshompod.github.io/b-10-assignment-5'
 })

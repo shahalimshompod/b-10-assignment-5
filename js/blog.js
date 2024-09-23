33df3c9ee86a7cc9276d13c7c9ac143f337f65e0
shahalimshompod <shahalimsompod@gmail.com>
2024-09-23 18:06:09 +0600

    fixed some major github 404 indexing error

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,3 @@
 document.getElementById('home-btn').addEventListener('click', function(){
-    window.location.href = '../index.html'
+    window.location.href = 'https://shahalimshompod.github.io/b-10-assignment-5/'
 })
